b66512271f00d24467e4586769371f6f1c4bd0b7
SharvariTeje <91945905+SharvariTeje@users.noreply.github.com>
2021-10-05 14:58:26 +0530

    Update helloworld.py
    Added the last name of the user.

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,4 @@
 first_name = input("Please enter your name:")
+last_name = input("Enter your last name:")
 
-print("Welcome to the demo",first_name)
+print("Welcome to the demo",first_name, ' ', last_name)
